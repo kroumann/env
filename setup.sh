20f4589c6e3aca9f24e3a4604643780a49ff9e92
kroumann <kourouma@DebianDevOs.home>
2018-01-05 07:38:04 +0100

    setup file for install. run this

--- setup.sh
+++ setup.sh
@@ -23,7 +23,12 @@
 
 PROGNAME=${0##*/}
 VERSION="0.1"
-
+dir_env="$PWD/env"
+dir_elnx="$PWD/elnx"
+user=${SUDO_USER:-$(whoami)}
+HOME_BASHRC="/home/$user/.bashrc"
+RT_BASHRC="/root/.bashrc"
+src_cfg_file="source $PWD/configs.sh"
 clean_up() { # Perform pre-exit housekeeping
   return
 }
@@ -72,6 +77,40 @@ _EOF_
   return
 }
 
+bashrc_install() {
+
+	if ! grep -Fq "$1" "$2";
+	then
+		echo $1 >> $2
+		echo "$2: updated"
+	else
+		echo "${1#* }: installed realoading..." 
+	fi
+}
+
+
+bin_file_inst() {
+	echo "installing ${1##*/} files..."
+	for fl in $1/*; do 
+		fname=${fl##*/}
+		if [ -f $fl ]; then
+			if [ ! -f $PWD/bin/${fname%.sh} ]; then
+				ln -s $fl $PWD/bin/${fname%.sh}
+			fi
+		fi
+	done
+}
+
+bin_folder_inst() {
+# assuming only 1 level of nested folders adopted
+echo "installing $1 subfolders..."
+	for fl in $1/*; do 
+		if [ -d $fl ]; then
+			bin_file_inst $fl
+		fi
+	done
+}
+
 # Trap signals
 trap "signal_exit TERM" TERM HUP
 trap "signal_exit INT"  INT
@@ -88,16 +127,52 @@ while [[ -n $1 ]]; do
       help_message; graceful_exit ;;
     -f | --force)
       echo "install all features without interaction" ;;
+#	  NOINTERACT=1;;
     -* | --*)
       usage
       error_exit "Unknown option $1" ;;
-    *)
-      echo "Argument $1 to process..." ;;
   esac
   shift
 done
 
 # Main logic
+
+# install bin files ...
+if ! grep -Fq -- "export PATH=$PWD/bin:\$PATH" configs.sh; then
+
+	echo "installing bin files ..."
+	echo "export PATH=$PWD/bin:\$PATH" >> configs.sh
+else
+	echo "bin files already installed."
+fi
+
+# install env files
+echo "installing env files..."
+if ! grep -Fq -- "for fl in $PWD/env/*; do source \$fl; done" configs.sh; then
+	echo "for fl in $PWD/env/*; do source \$fl; done" >> configs.sh
+fi
+
+# install embedded linux utils scripts
+bin_folder_inst "$PWD/elnx"
+bin_file_inst "$PWD/elnx"
+
+# install utilities scripts
+bin_folder_inst "$PWD/utils"
+bin_file_inst "$PWD/utils"
+
+# install tools configuration scripts
+bin_folder_inst "$PWD/tools"
+bin_file_inst "$PWD/tools"
+
+# vim environment install
+
+# tmux env install
+
+echo "installing configs in bashrc files..."
+bashrc_install "$src_cfg_file" "$HOME_BASHRC"
+source $HOME_BASHRC
+bashrc_install "$src_cfg_file" "$RT_BASHRC"
+source $RT_BASHRC
 
 graceful_exit
 
