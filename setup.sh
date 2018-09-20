ae4c306b056f701deaa375ff444b491479b88e5a
kourouma <kroumann@gmail.com>
2018-09-20 06:52:38 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -30,9 +30,9 @@ HOME_BASHRC="/home/$user/.bashrc"
 RT_BASHRC="/root/.bashrc"
 src_cfg_file="source $PWD/configs.sh"
 tmux_alias_fl="source $PWD/tmux/tmux_aliases.sh"
-tmux_fl="~/.tmux.conf"
+tmux_fl="/home/$user/.tmux.conf"
 vimrc_fl="/home/$user/.vimrc"
-git_fl="~/.gitconfig"
+git_fl="/home/$user/.gitconfig"
 clean_up() { # Perform pre-exit housekeeping
   return
 }
@@ -86,15 +86,15 @@ bashrc_install() {
 	if ! grep -Fq "$1" "$2";
 	then
 		echo $1 >> $2
-		echo "$2: updated"
+		echo "$2: installed"
 	else
-		echo "${1#* }: installed realoading..." 
+		echo "${1#* }: reloaded" 
 	fi
 }
 
 
 bin_file_inst() {
-	echo "installing ${1##*/} files..."
+	echo -n "installing ${1##*/} files... "
 	for fl in $1/*; do 
 		fname=${fl##*/}
 		if [ -f $fl ]; then
@@ -103,16 +103,18 @@ bin_file_inst() {
 			fi
 		fi
 	done
+	echo "done"
 }
 
 bin_folder_inst() {
 # assuming only 1 level of nested folders adopted
-echo "installing $1 subfolders..."
+echo "installing $1 subfolders... "
 	for fl in $1/*; do 
 		if [ -d $fl ]; then
 			bin_file_inst $fl
 		fi
 	done
+echo "done"
 }
 
 # Trap signals
@@ -148,18 +150,19 @@ fi
 # install bin files ...
 if ! grep -Fq -- "export PATH=$PWD/bin:\$PATH" configs.sh; then
 
-	echo "installing bin files ..."
+	echo -n "installing bin files ... "
 	echo "export PATH=$PWD/bin:\$PATH" >> configs.sh
+	echo "done"
 else
-	echo "bin files already installed."
+	echo "already installed."
 fi
 
 # install env files
-echo "installing env files..."
+echo -n "installing env files... "
 if ! grep -Fq -- "for fl in $PWD/env/*; do source \$fl; done" configs.sh; then
 	echo "for fl in $PWD/env/*; do source \$fl; done" >> configs.sh
 fi
-
+echo "done"
 # install embedded linux utils scripts
 bin_folder_inst "$PWD/elnx"
 bin_file_inst "$PWD/elnx"
@@ -173,25 +176,31 @@ bin_folder_inst "$PWD/tools"
 bin_file_inst "$PWD/tools"
 
 # vim environment install
+echo -n "installing vim files... "
 if [ ! -f $vimrc_fl ]; then
-	ln -sf $PWD/vim/vimrc $vimrc_fl	
+	ln -sf $PWD/vim/vimrc $vimrc_fl
+	echo "done"
 else
-	echo "vimrc already installed."	
+	echo "already installed."	
 fi
 
 # tmux env install
-if [ ! -f $tmux_fl]; then
+echo -n "installing tmux files... "
+if [ ! -f $tmux_fl ]; then
 	ln -sf $PWD/tmux/tmux.conf $tmux_fl	
-	bashrc_install "tmux_alias_fl" "$HOME_BASHRC"
+	echo "$tmux_alias_fl" >> configs.sh
+	echo "done"
 else
-	echo "tmux configs already installed."	
+	echo "already installed."	
 fi
 
 # git configuration file install 
+echo -n "installing gitconfig file ... "
 if [ ! -f $git_fl ]; then
 	ln -sf $PWD/git/gitconfig $git_fl
+	echo "done"
 else
-	echo "gitconfig already installed."	
+	echo "already installed."	
 fi
 
 
